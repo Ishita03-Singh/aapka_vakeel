1dd6da9781eaa1e561cc080add0c34edc38dc3d1
Ishita Singh <ishitasingh@Ishitas-Laptop.local>
2024-10-06 01:27:22 +0530

    apiservice commented

--- lib/screens/chatGPT/apiService.dart
+++ lib/screens/chatGPT/apiService.dart
@@ -4,24 +4,24 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class ApiService {
-  final String code = ''; // Replace with your OpenAI API key
+//   final String code = ''; // Replace with your OpenAI API key
 
-  Future<String> fetchChatGPTResponse(String message) async {
-  try {
-    final url = Uri.parse('https://api.openai.com/v1/chat/completions');
-    final response = await http.post(
-      url,
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': 'Bearer $code',
-      },
-      body: jsonEncode({
-        'model': 'gpt-3.5-turbo',
-        'messages': [
-          {'role': 'user', 'content': message},
-        ],
-      }),
-    );
+//   Future<String> fetchChatGPTResponse(String message) async {
+//   try {
+//     final url = Uri.parse('https://api.openai.com/v1/chat/completions');
+//     final response = await http.post(
+//       url,
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'Authorization': 'Bearer $code',
+//       },
+//       body: jsonEncode({
+//         'model': 'gpt-3.5-turbo',
+//         'messages': [
+//           {'role': 'user', 'content': message},
+//         ],
+//       }),
+//     );
 
 //     if (response.statusCode == 200) {
 //       final jsonResponse = jsonDecode(response.body);
@@ -34,11 +34,6 @@ class ApiService {
 //     print('Caught an exception: $e');
 //     rethrow; // or handle it accordingly
 //   }
-// }
-  } catch (e) {
-    print('Caught an exception: $e');
-     rethrow; // or handle it accordingly
-   }
 
-}
+// }
 }
